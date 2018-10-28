3ff47cfaf6c6b0be587346f34364d338182bcbde
Benjamin Fischer <benjamin.f.fischer@fau.de>
2018-10-28 23:44:27 +0100

    Modified Photo. It now contains a location attribute.

--- src/main/java/org/wahlzeit/model/Photo.java
+++ src/main/java/org/wahlzeit/model/Photo.java
@@ -66,6 +66,8 @@ public class Photo extends DataObject {
 	public static final int MAX_PHOTO_HEIGHT = 600;
 	public static final int MAX_THUMB_PHOTO_WIDTH = 105;
 	public static final int MAX_THUMB_PHOTO_HEIGHT = 150;
+	
+	public Location location;
 
 	protected PhotoId id = null;
 	
@@ -148,6 +150,15 @@ public class Photo extends DataObject {
 		incWriteCount();
 	}
 
+	public Location getLocation() {
+		return this.location;
+	}
+	
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	
 	/**
 	 * @methodtype get
 	 */
